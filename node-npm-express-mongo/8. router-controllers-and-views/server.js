7130c210d05f62091d5a05c87b73e4a02ab6ba39
Matheus Matos <57081161+mmdes@users.noreply.github.com>
2024-04-16 19:25:56 -0400

    Implementing view

---
+++ node-npm-express-mongo/8. router-controllers-and-views/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express()
+const routes = require('./routes');
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }));
+app.set('views', path.resolve(__dirname,'src', 'views'))
+app.set('view engine', 'ejs')
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log('Server is running at port 3000');
+    console.log('Access http://localhost:3000/');
+});
